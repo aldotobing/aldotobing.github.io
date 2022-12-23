8cc1796484c44718a859564751c453aaed90eb31
Aldo Tobing <aldo_tobing@hotmail.com>
2022-12-23 10:24:05 +0700

    music add

--- tiwi/script/main.js
+++ tiwi/script/main.js
@@ -1,3 +1,8 @@
+//Music
+<audio autoplay="autoplay">
+  <source src="https://github.com/AnshumanMahato/Happy-Birthday-Card/blob/main/src/resources/sfx/hbd.mp3" />
+</audio>;
+
 // Import the data to customize and insert them into page
 const fetchData = () => {
   fetch("/tiwi/customize.json")
