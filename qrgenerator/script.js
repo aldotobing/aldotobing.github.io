012a9bacfb25b47bc9b1b05d80f11b6544a929d2
Aldo Tobing <aldo_tobing@hotmail.com>
2023-06-07 13:09:59 +0700

    com

--- qrgenerator/script.js
+++ qrgenerator/script.js
@@ -32,7 +32,7 @@ input.addEventListener("input", function () {
   // Set a timeout of 1 second
   setTimeout(function () {
     // Remove the loading message
-    qrCode.querySelector(qr).removeChild(loading);
+    qrCode.querySelector("div").removeChild(loading);
 
     // Generate the QR code with the input value
     qrcode.makeCode(data);
