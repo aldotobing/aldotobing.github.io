6cd41095037049c90093489a125f7dded4461866
Aldo Tobing <aldo_tobing@hotmail.com>
2023-06-07 13:52:28 +0700

    com

--- qrgenerator/script.js
+++ qrgenerator/script.js
@@ -1,24 +1,27 @@
-function generateQRCode(data) {
-  var loadingMessage = document.getElementById("loadingMessage");
-  loadingMessage.style.display = "block";
-  loadingMessage.innerHTML = "Generating QR...";
-  var qrcode = new QRCode("qrcode", {
-    text: data,
-    width: 200,
-    height: 200,
-  });
-  setTimeout(function () {
-    loadingMessage.style.display = "none";
-    qrcode.show();
-  }, 1000);
-}
+// Elements
+const qrdata_elm = document.querySelector("#qr-data");
+const qrcode_elm = document.querySelector("#qr-code");
 
-function smoothLoading() {
-  var input = document.getElementById("qr-data");
-  input.addEventListener("input", function () {
-    var data = input.value;
-    generateQRCode(data);
-  });
-}
+// QRCode
+const qrcode = new QRCode(qrcode_elm);
+// QR-Code generator
+const generateQrCode = (value) => {
+  // Show loading message
+  const loadingMessage = document.createElement("div");
+  loadingMessage.textContent = "Generating QR code...";
+  document.body.appendChild(loadingMessage);
 
-smoothLoading();
+  // Generate QR code
+  qrcode.makeCode(value);
+
+  // Remove loading message
+  loadingMessage.remove();
+};
+
+// Events
+qrdata_elm.addEventListener("change", () => {
+  generateQrCode(qrdata_elm.value);
+});
+qrdata_elm.addEventListener("input", () => {
+  generateQrCode(qrdata_elm.value);
+});
