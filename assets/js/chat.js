0b0a8e86d194f6759e83678e990fa4658961a6c7
Aldo Tobing <31465898+aldotobing@users.noreply.github.com>
2024-12-17 15:48:50 +0700

    Update chat.js

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -131,7 +131,7 @@ document.addEventListener("DOMContentLoaded", function () {
   addMessage(userInput, "user-message");
 
   // Static bot response with a link
-  const staticResponse = 'The AI has been moved to: <a href="https://ai.aldotobing.online" target="_blank" rel="noopener noreferrer">https://kapal-lawd-ai.pages.dev</a>';
+  const staticResponse = 'The AI has been moved to: <a href="https://ai.aldotobing.online" target="_blank" rel="noopener noreferrer">https://ai.aldotobing.online</a>';
   
   // Use the typing effect with the static response
   await typeWriterEffect(staticResponse, () => {
